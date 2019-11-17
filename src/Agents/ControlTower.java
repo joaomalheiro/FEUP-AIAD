7e7b22898d958bac898e0ce8431f3e0a214f3e17
Pedro Lopes <up201603557@fe.up.pt>
2019-11-17 22:14:50 +0000

    Fixed DF registration bug

--- src/Agents/ControlTower.java
+++ src/Agents/ControlTower.java
@@ -9,31 +9,34 @@ import gui.AirportGUI;
 import jade.core.*;
 import jade.domain.DFService;
 import jade.domain.FIPAAgentManagement.DFAgentDescription;
+import jade.domain.FIPAAgentManagement.SearchConstraints;
 import jade.domain.FIPAAgentManagement.ServiceDescription;
 import jade.domain.FIPAException;
 import jade.lang.acl.ACLMessage;
 import jade.core.behaviours.CyclicBehaviour;
+import jade.lang.acl.MessageTemplate;
 import jade.proto.SubscriptionInitiator;
 
+import javax.naming.ldap.Control;
 import java.util.*;
 
-public class ControlTower extends Agent{
+public class ControlTower extends Agent {
 
     Comparator<AirplaneInfo> airplaneComparator = new Comparator<AirplaneInfo>() {
-        @Override public int compare(AirplaneInfo a1, AirplaneInfo a2) {
-            if(!a1.getLocalName().equals(a2.getLocalName())){
-                if(a1.getTimeToTower() > a2.getTimeToTower())
+        @Override
+        public int compare(AirplaneInfo a1, AirplaneInfo a2) {
+            if (!a1.getLocalName().equals(a2.getLocalName())) {
+                if (a1.getTimeToTower() > a2.getTimeToTower())
                     return 1;
-                else if(a1.getFuel() < 5 && a1.getFuel() < a2.getFuel())
+                else if (a1.getFuel() < 5 && a1.getFuel() < a2.getFuel())
                     return 1;
-                else if (companyPriorities.get(a1.getLocalName().replaceAll("\\d","")) > companyPriorities.get(a2.getLocalName().replaceAll("\\d","")))
+                else if (companyPriorities.get(a1.getLocalName().replaceAll("\\d", "")) > companyPriorities.get(a2.getLocalName().replaceAll("\\d", "")))
                     return 1;
                 else if (a1.getPassengers() > a2.getPassengers())
                     return 1;
                 else
                     return -1;
-            }
-            else
+            } else
                 return 0;
         }
     };
@@ -54,7 +57,7 @@ public class ControlTower extends Agent{
         this.passenger_vehicles = new Vector<>();
         this.passenger_vehicles_availability = new Vector<>();
         map = new Character[20][20];
-        for (Character[] row: map)
+        for (Character[] row : map)
             Arrays.fill(row, '*');
 
         map[9][0] = 'C';
@@ -97,52 +100,98 @@ public class ControlTower extends Agent{
         return dfd;
     }
 
-    private void initialPassengerVehicleSearch(){
+    private void initialPassengerVehicleSearch() {
         DFAgentDescription dfd = passengerVehicleDFAgentDescriptorCreator();
 
         try {
             DFAgentDescription[] search_result = DFService.search(this, dfd);
 
-            for(DFAgentDescription vehicle : search_result) {
+            for (DFAgentDescription vehicle : search_result) {
                 System.out.println(this.passenger_vehicles.add(vehicle.getName()));
                 this.passenger_vehicles_availability.add(new Pair<>(vehicle.getName().getLocalName(), false));
             }
-            } catch (FIPAException fe) {
+
+        } catch (FIPAException fe) {
             fe.printStackTrace();
         }
     }
 
     private void subscribePassengerVehicleAgent() {
         DFAgentDescription template = passengerVehicleDFAgentDescriptorCreator();
-        
-        addBehaviour( new SubscriptionInitiator( this,
-                DFService.createSubscriptionMessage( this, getDefaultDF(),
-                        template, null))
-        {
+        SearchConstraints sc = new SearchConstraints();
+        sc.setMaxResults(new Long(1));
+
+        send(DFService.createSubscriptionMessage(this, getDefaultDF(),
+                template, sc));
+
+        addBehaviour(new RegistrationNotification(this));
+    }
+
+    class RegistrationNotification extends CyclicBehaviour {
+
+        private ControlTower ct;
+
+        public RegistrationNotification(ControlTower ct) {
+            super();
+            this.ct = ct;
+        }
+
+        public void action() {
+            ACLMessage msg = receive(MessageTemplate.MatchSender(getDefaultDF()));
+
+            if (msg != null) {
+
+                try {
+                    DFAgentDescription[] dfds = DFService.decodeNotification(msg.getContent());
+
+                    if (dfds.length > 0) {
+                        for (DFAgentDescription dfd : dfds) {
+                            AID new_agent = dfd.getName();
+                            if (!ct.getPassenger_vehicles().contains(new_agent)) {
+                                ct.getPassenger_vehicles().add(new_agent);
+                                ct.getPassenger_vehicles_availability().add(new Pair<>(new_agent.getLocalName(), false));
+                                System.out.println("New passenger vehicle on duty: " + new_agent.getLocalName());
+                            }
+                        }
+                    }
+                } catch (FIPAException e) {
+                    e.printStackTrace();
+                }
+
+            }
+
+            block();
+
+        }
+    }
+
+        /* addBehaviour(new SubscriptionInitiator(this,
+                DFService.createSubscriptionMessage(this, getDefaultDF(),
+                        template, null)) {
             protected void handleInform(ACLMessage inform) {
                 try {
                     DFAgentDescription[] dfds =
                             DFService.decodeNotification(inform.getContent());
-
+                    System.out.println("my class name is " + myAgent.getLocalName());
                     ControlTower ct = (ControlTower) myAgent;
-                    for(DFAgentDescription dfd : dfds) {
+                    for (DFAgentDescription dfd : dfds) {
                         AID new_agent = dfd.getName();
-                        if(!ct.getPassenger_vehicles().contains(new_agent)) {
+                        if (!ct.getPassenger_vehicles().contains(new_agent)) {
                             ct.getPassenger_vehicles().add(new_agent);
                             ct.getPassenger_vehicles_availability().add(new Pair<>(new_agent.getLocalName(), false));
                             System.out.println("New passenger vehicle on duty: " + new_agent.getLocalName());
                         }
                     }
 
+                } catch (FIPAException fe) {
+                    fe.printStackTrace();
                 }
-                catch (FIPAException fe) {fe.printStackTrace(); }
             }
-        });
-    }
+        });*/
 
 
     public void pushAirplane(AirplaneInfo airplane) {
-        int y= -1;
+        int y = -1;
         for (AirplaneInfo value : airplanes) {
             if (value.getLocalName().equals(airplane.getLocalName())) {
                 y = value.getY();
@@ -158,23 +207,24 @@ public class ControlTower extends Agent{
 
         System.out.println();
 
-        if(y != -1) {
+        if (y != -1) {
             map[10 - airplane.getTimeToTower()][y] = 'A';
             airplane.setX(10 - airplane.getTimeToTower());
             airplane.setY(y);
         } else {
             map[10 - airplane.getTimeToTower()][currLine] = 'A';
-            airplane.setX(10-airplane.getTimeToTower());
+            airplane.setX(10 - airplane.getTimeToTower());
             airplane.setY(currLine);
 
-            if(currLine < 19)
+            if (currLine < 19)
                 currLine++;
             else currLine = 0;
         }
 
         gui.getPanel().repaint();
         gui.getPanel().setFocusable(true);
-        gui.getPanel().requestFocusInWindow();;
+        gui.getPanel().requestFocusInWindow();
+        ;
 
         // Loop over the TreeSet values
         // and print the values
@@ -182,15 +232,16 @@ public class ControlTower extends Agent{
         while (iterator.hasNext())
             System.out.print(iterator.next()
                     + ", ");
-        System.out.println(); }
+        System.out.println();
+    }
 
-    public void landAirplane(AirplaneInfo airplane){
-        addBehaviour(new AirplaneLanded(airplane,10 - companyPriorities.get(airplane.getLocalName().replaceAll("\\d",""))));
-        airplanes.removeIf(a1 -> a1.getLocalName().equals(airplane.getLocalName()) );
+    public void landAirplane(AirplaneInfo airplane) {
+        addBehaviour(new AirplaneLanded(airplane, 10 - companyPriorities.get(airplane.getLocalName().replaceAll("\\d", ""))));
+        airplanes.removeIf(a1 -> a1.getLocalName().equals(airplane.getLocalName()));
     }
 
     public void setPriority(String companyName, int priority) {
-        companyPriorities.put(companyName,priority);
+        companyPriorities.put(companyName, priority);
     }
 }
 
